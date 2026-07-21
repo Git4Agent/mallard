af20e1a2349d27f11a6eef3ab897149ef554fa58
Fuyao Li <fuyaoli2018@gmail.com>
2026-07-21 11:46:53 -0700

    Remove updater infrastructure

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,8 +12,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             activity_log::query_activity_logs,
             activity_log::get_activity_log_stats,
